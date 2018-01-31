69cc81597883eabca5fcf0648b1efdd2128a0d67
suncheng <suncheng@kuaishou.com>
2018-01-31 13:54:30 +0800

    fix find class window not popup

--- platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
+++ platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
@@ -922,7 +922,7 @@ public class AbstractPopup implements JBPopup {
     window.setFocusableWindowState(myRequestFocus);
     window.setFocusable(myRequestFocus);
     // temporary w/a (will be brought back in jdk)
-    window.setType(Window.Type.POPUP);
+    //window.setType(Window.Type.POPUP);
     // Swing popup default always on top state is set in true
     window.setAlwaysOnTop(false);
 
